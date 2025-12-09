56773dc201e78690f19a8fb9aa6674a63b79e7be
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-09 23:34:14 +0200

    Refactor ORM Fundamentals to K&R style

---
+++ Exercise - ORM Fundamentals/Solutions/SoftUniClientApp/SoftUniDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace MyDemoApp;
+
+using MiniORM;
+using SoftUniClientApp.Models;
+
+public class SoftUniDbContext : DbContext {
+    public SoftUniDbContext(string connectionString) : base(connectionString) {
+
+    }
+
+    public DbSet<Address> Addresses { get; set; }
+
+    public DbSet<Department> Departments { get; set; }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<EmployeeProject> EmployeesProjects { get; set; }
+
+    public DbSet<Project> Projects { get; set; }
+
+    public DbSet<Town> Towns { get; set; }
+}
+
